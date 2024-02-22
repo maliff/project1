e93c8b9a8906c572ccf38590b18adddc634b0db3
Muhd Aliff <muhd.aliffuddeen@gmail.com>
2024-02-22 17:02:49 +0800

    Account Management Module
    Create New Data provider Form

--- src/views/NEBAccountManagement/CreateNewDataProvider.js
+++ src/views/NEBAccountManagement/CreateNewDataProvider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   faArrowUp,
   faBars,
@@ -16,8 +16,8 @@ import {
   faTimes,
   faUserPlus,
   faUsers,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Badge,
   Button,
@@ -33,84 +33,246 @@ import {
   InputGroupAddon,
   Row,
   Table,
-} from 'reactstrap';
-import LteContent from '../../components/LteContent';
-import LteContentHeader from '../../components/LteContentHeader';
+  Label,
+} from "reactstrap";
+import LteContent from "../../components/LteContent";
+import LteContentHeader from "../../components/LteContentHeader";
 
 const CreateNewDataProvider = () => {
   return (
     <>
-    <LteContentHeader title='Create New Data Provider' />
-    <LteContent>
-    <div className="row">
-  <div className="col-md-12">
-    <div className="card card-default">
-      <div className="card-header">
-        <h3 className="card-title">bs-stepper</h3>
-      </div>
-      <div className="card-body p-0">
-        <div className="bs-stepper">
-          <div className="bs-stepper-header" role="tablist">
-            {/* your steps here */}
-            <div className="step" data-target="#logins-part">
-              <button type="button" className="step-trigger" role="tab" aria-controls="logins-part" id="logins-part-trigger">
-                <span className="bs-stepper-circle">1</span>
-                <span className="bs-stepper-label">Logins</span>
-              </button>
-            </div>
-            <div className="line" />
-            <div className="step" data-target="#information-part">
-              <button type="button" className="step-trigger" role="tab" aria-controls="information-part" id="information-part-trigger">
-                <span className="bs-stepper-circle">2</span>
-                <span className="bs-stepper-label">Various information</span>
-              </button>
-            </div>
-          </div>
-          <div className="bs-stepper-content">
-            {/* your steps content here */}
-            <div id="logins-part" className="content" role="tabpanel" aria-labelledby="logins-part-trigger">
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+      <LteContentHeader title="Create New Data Provider" />
+      <LteContent>
+        {/* Main content */}
+        <section className="content">
+          <div className="container-fluid">
+            <div className="card card-default">
+              <div className="card-header">
+                <h2
+                  className="card-title"
+                  style={{ fontSize: "24px", fontWeight: "bold" }}
+                >
+                  Create Profile
+                </h2>
+                <div style={{ clear: "both" }}></div>{" "}
+                <h6>Fill up all details*</h6>
               </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-              </div>
-              <button className="btn btn-primary" onclick="stepper.next()">Next</button>
-            </div>
-            <div id="information-part" className="content" role="tabpanel" aria-labelledby="information-part-trigger">
-              <div className="form-group">
-                <label htmlFor="exampleInputFile">File input</label>
-                <div className="input-group">
-                  <div className="custom-file">
-                    <input type="file" className="custom-file-input" id="exampleInputFile" />
-                    <label className="custom-file-label" htmlFor="exampleInputFile">Choose file</label>
+              {/* /.card-header */}
+
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <h6>Data Provider's details</h6>
                   </div>
-                  <div className="input-group-append">
-                    <span className="input-group-text">Upload</span>
+                  <div className="col-lg-6">
+                    <h6>Contact Person details</h6>
+                  </div>
+                </div>
+                <div className="row">
+                  {/* 1 column */}
+                  <div className="col-lg-3">
+                    {/* <h6>Fill up all details*</h6> */}
+                    <div className="form-group">
+                      <label htmlFor="firstNameInput">First Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="firstNameInput"
+                        placeholder="Enter First Name"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="userNameInput">Username</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="userNameInput"
+                        placeholder="Enter Username"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="emailInput">Email</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="emailInput"
+                        placeholder="Enter email"
+                      />
+                    </div>
+                  </div>
+
+                  {/* 2 Column */}
+                  <div className="col-lg-3">
+                    <div className="form-group">
+                      <label htmlFor="lastNameInput">Last Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="lastNameInput"
+                        placeholder="Enter Last Name"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="phoneNumberInput">Phone Number</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="phoneNumberInput"
+                        placeholder="Enter Phone Number"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Division</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="divisionInput"
+                        placeholder="Enter Division"
+                      />
+                    </div>
+                  </div>
+
+                  {/* 3 Column */}
+                  <div className="col-lg-3">
+                    <div className="form-group">
+                      <label htmlFor="firstNameInput2">First Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="firstNameInput2"
+                        placeholder="Enter First Name"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="emailInput2">Email</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="emailInput2"
+                        placeholder="Enter email"
+                      />
+                    </div>
+                  </div>
+
+                  {/* 4 Column */}
+                  <div className="col-lg-3">
+                    <div className="form-group">
+                      <label htmlFor="lastNameInput2">Last Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="lastNameInput2"
+                        placeholder="Enter Last Name"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="phoneNumberInput2">Phone Number</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="phoneNumberInput2"
+                        placeholder="Enter Phone Number"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-              <button className="btn btn-primary" onclick="stepper.previous()">Previous</button>
-              <button type="submit" className="btn btn-primary">Submit</button>
+
+              {/* Agency Detail's */}
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <h6>Agency Detail's</h6>
+                  </div>
+                </div>
+                <div className="row">
+                  {/* 1 column */}
+                  <div className="col-lg-4">
+                    {/* <h6>Fill up all details*</h6> */}
+                    <div className="form-group">
+                      <label htmlFor="energySectorInput">Energy Sector</label>
+                      <select
+                        className="form-control select2"
+                        style={{ width: "100%" }}
+                        id="energySectorInput"
+                        name="energySectorInput"
+                      >
+                        <option selected>Please Select...</option>
+                        <option value="electricity">Electricity</option>
+                        <option value="coal">Coal</option>
+                        <option value="fuel">Fuel</option>
+                        <option value="bio-diesel">Bio-diesel</option>
+                        <option value="solar">Solar</option>
+                        <option value="petrol">Petrol</option>
+                      </select>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="addressInput">Address</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="addressInput1"
+                        placeholder="Enter Address 1"
+                      />
+                      <br />
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="addressInput2"
+                        placeholder="Enter Address 2"
+                      />
+                    </div>
+                  </div>
+
+                  {/* 2 Column */}
+                  <div className="col-lg-4">
+                    <div className="form-group">
+                      <label htmlFor="agencyInput">Agency</label>
+                      <select
+                        className="form-control select2"
+                        style={{ width: "100%" }}
+                        id="agencyInput"
+                        name="agencyInput"
+                      >
+                        <option selected>Please Select...</option>
+                        <option value="TNB">Tenaga Nasional Berhad</option>
+                        <option value="petronas">Petronas</option>
+                        <option value="shell">Shell</option>
+                        <option value="tesla">Tesla</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  {/* 3 Column */}
+                  <div className="col-lg-4">
+                    <div className="form-group">
+                      <label htmlFor="phoneNumberInput3">Phone Number</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="phoneNumberInput3"
+                        placeholder="Enter Phone Number"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Save and Cancel Buttons */}
+            <div className="d-flex justify-content-end mt-3 mb-3">
+              <Button color="secondary" className="mr-2">
+                Cancel
+              </Button>
+              <Button color="primary">Save</Button>
             </div>
           </div>
-        </div>
-      </div>
-      {/* /.card-body */}
-      <div className="card-footer">
-        Visit <a href="https://github.com/Johann-S/bs-stepper/#how-to-use-it">bs-stepper documentation</a> for more examples and information about the plugin.
-      </div>
-    </div>
-    {/* /.card */}
-  </div>
-</div>
+          {/* /.container-fluid */}
+        </section>
+        {/* /.content */}
+      </LteContent>
+    </>
+  );
+};
 
-
-    </LteContent>
-  </>
-  )
-}
-
-export default CreateNewDataProvider
+export default CreateNewDataProvider;
